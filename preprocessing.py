311789dbab32fa59cee9fada18a7e7b9d93ddb09
keenjo <90385673+keenjo@users.noreply.github.com>
2022-10-25 14:56:39 +0200

    creation of vectorized inputs

--- preprocessing.py
+++ preprocessing.py
@@ -1,40 +1,183 @@
 import spacy
+import pandas as pd
+import os
+import json
+from tqdm import tqdm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
 
 nlp = spacy.load('en_core_web_sm')
 
-# * Update later to go through all of the *.final files in the dataset repo
+directory = '.../terminology-project-2022' # Path to the annotated terminology project data
+data_path = '/.../' # Path where you want the preprocessed data to be saved
 
-fpath = 'Insert path of file you want to preprocess'
+# Lists for each of the dataset splits: train, dev, test
+train = []
+dev = []
+test = []
+splits = [train, dev, test]
 
-with open(fpath, 'r+') as f:
-    text = f.readlines()
+# List to iterate through the dataset split files for preprocessing
+splits_name = ['train', 'dev', 'test']
 
-words = [] # all of the words
-labels = [] # all of the labels
-POS_list = [] # all of the word POS tags
+# Dictionary which will contain the inputs for each dataset split
+data_total = {'train': [],
+              'dev': [],
+              'test': []}
 
-# Splitting the txt file into the word and label lists
-for line in text:
-    if line != '\n':
-        word, label = line.split('\t')
-        words.append(word)
-        labels.append(label.strip('\n'))
+# Dictionary which will contain the annotated tags for each dataset split
+combined_tags = {'train': [],
+                'dev': [],
+                'test': []}
 
-# Getting the POS tags of all of the words
-doc = nlp(' '.join(words))
-for token in doc:
-    POS_list.append(token.pos_)
 
-# Organizing each input word into a dictionary with several features
-inputs = [] # List containing all of the input words along with all of their features
+# Collecting all of the final annotation files
+for root, dirs, files in os.walk(directory):
+    for file in sorted(files):
+        if file.endswith('.final'):
+            if 'train' in root:
+                train.append(os.path.join(root, file))
+            elif 'dev' in root:
+                dev.append(os.path.join(root, file))
+            elif 'test' in root:
+                test.append(os.path.join(root, file))
 
-for index, word in enumerate(words):
-    inp = {}
-    inp['Word'] = word
-    inp['Word POS'] = POS_list[index]
-    inp['Preceding'] = '<bos' if index == 0 else words[index - 1]
-    inp['Preceding POS'] = '---' if index == 0 else POS_list[index - 1]
-    inp['Following'] = '<eos>' if index == len(words) - 1 else words[index + 1]
-    inp['Following POS'] = '---' if index == len(words) - 1 else POS_list[index + 1]
 
-    inputs.append(inp)
+# Preprocessing loop
+for split_index, split in enumerate(splits):
+    split_tags = []
+    for fpath in tqdm(split):
+        #print(fpath)
+        with open(fpath, 'r+') as f:
+            text = f.readlines()
+
+        words = [] # all of the words for a given document
+        doc_tags = [] # all of the term tags for a given document
+        POS_list = [] # all of the word POS tags for a given document
+
+        # Splitting the txt file into the word and tag lists
+        for line in text:
+            #print(line)
+            if line != '\n' and line != '':
+                try:
+                    word, tag = line.split('\t')
+                except ValueError:
+                    word, tag = line.split(' ')
+                tag = ''.join(tag.split())
+                if word != '' and tag != '':
+                    words.append(word)
+                    doc_tags.append(tag)
+                elif word == '' and tag == '':
+                    continue
+                elif word != '' and tag == '':
+                    words.append(word)
+                    doc_tags.append('O')
+
+        # Getting the POS tags of all of the words
+        doc = nlp(' '.join(words))
+        for token in doc:
+            POS_list.append(token.pos_)
+
+        # Appending each input word into the inputs dictionary
+        for index, word in enumerate(words):
+            # Inputs dictionary: contains all input information for a given split
+            inputs = {}
+            # Adding all of the information to the inputs dictionary
+            inputs['Main word'] = word
+            inputs['Main POS'] = POS_list[index]
+            inputs['is_first'] = 'True' if index == 0 else 'False'
+            inputs['is_capitalized'] = 'True' if word.lower() != word else 'False'
+            #inputs['Word Tag'] = labels[index]
+            inputs['Preceding word'] = '<bos>' if index == 0 else words[index - 1]
+            inputs['Preceding POS'] = '---' if index == 0 else POS_list[index - 1]
+            inputs['Preceding Tag'] = '---' if index == 0 else doc_tags[index - 1]
+            inputs['Following word'] = '<eos>' if index == len(words) - 1 else words[index + 1]
+            inputs['Following POS'] = '---' if index == len(words) - 1 else POS_list[index + 1]
+            inputs['Following Tag'] = '---' if index == len(words) - 1 else doc_tags[index + 1]
+            data_total[splits_name[split_index]].append(inputs)
+
+        # Adding the tags from one document to list of tags for data split (train, dev, or test)
+        for tag in doc_tags:
+            split_tags.append(tag)
+    # Adding the tags from data split to a combined_tags dictionary
+    for tag in split_tags:
+        combined_tags[splits_name[split_index]].append(tag)
+
+
+# Vectorizing the data
+# Creating a vocabulary for all of the data
+vocab = []
+# Used separate lists here just to organize things a bit more
+just_words = [] # List of all unique words
+just_tags = [] # List of all unique term tags
+just_pos = [] # List of all unique POS tags
+just_bool = ['False', 'True']
+
+for split in data_total:
+    for input in data_total[split]:
+        for key in input:
+            vocab.append(input[key])
+            if 'word' in key.lower():
+                just_words.append(input[key])
+            elif 'pos' in key.lower():
+                just_pos.append(input[key])
+            elif 'tag' in key.lower():
+                just_tags.append(input[key])
+
+just_words = list(set(just_words))
+just_pos = list(set(just_pos))
+just_tags = list(set(just_tags))
+# Converting the vocabulary to token:integer dictionary
+vocab = just_bool + just_tags + just_pos + just_words
+vocab_dict = {}
+for index, item in enumerate(vocab):
+    vocab_dict[item] = index
+
+# Updating the data and tag dictionaries with the vocabulary integer values
+for split in data_total:
+    for input in data_total[split]:
+        for key in input:
+            input[key] = vocab_dict[input[key]]
+
+for split in combined_tags:
+    for index, tag in enumerate(combined_tags[split]):
+        combined_tags[split][index] = vocab_dict[tag]
+
+
+# Saving the data (inputs, tags, vocabulary) as json files
+with open(f'{data_path}/inputs.json', 'w+') as file:
+    json.dump(data_total, file)
+
+with open(f'{data_path}/tags.json', 'w+') as file:
+    json.dump(combined_tags, file)
+
+with open(f'{data_path}/vocab.json', 'w+') as file:
+    json.dump(vocab_dict, file)
+
+print(f'All inputs, tags, and vocabulary saved to {data_path}')
+
+
+# ************************************************************
+# ************************************************************
+
+# Testing the data on a Decision Tree Classifier
+#classifier = Pipeline([
+    #('vectorizer', DictVectorizer()),
+    #('classifier', DecisionTreeClassifier())
+#])
+'''
+vectorizer = DictVectorizer()
+train_vect = vectorizer.fit_transform(data_total['train'])
+test_vect = vectorizer.fit_transform(data_total['test'])
+print(train_vect.shape)
+print(test_vect.shape)
+#reformed_data = vectorizer.inverse_transform(train_vect)
+
+classifier = DecisionTreeClassifier()
+
+classifier.fit(train_vect, combined_tags['train'])
+
+res_test = classifier.score(test_vect, combined_tags['test'])
+print(f'Accuracy test: {res_test}')
+'''
