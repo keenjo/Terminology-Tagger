48c51417fdb5ee155e4f8478d82ec296355acf3d
Joe Keenan <90385673+keenjo@users.noreply.github.com>
2022-12-17 14:14:03 +0100

    Update RFC_classif.py

--- RFC_classif.py
+++ RFC_classif.py
@@ -5,20 +5,22 @@ import numpy as np
 
 directory = 'terminology-project-2022/' # Path to the annotated terminology project data
 
-#split = 'train' # train, test, or dev
 print('Loading data...')
 train_data = TermsDataset(directory, 'train', one_hot=True)
 test_data = TermsDataset(directory, 'test', one_hot=True)
 dev_data = TermsDataset(directory, 'dev', one_hot=True)
 
+# Unzipping the data
 train_split = list(zip(*train_data))
 test_split = list(zip(*test_data))
 dev_split = list(zip(*dev_data))
 
+# Converting the train data from tensors to numpy arrays
 train_data, train_labels = train_split
 train_data = [np.array(x) for x in train_data]
 train_labels = [np.array(x) for x in train_labels]
 
+# Converting the train data from tensors to numpy arrays
 test_data, test_labels = test_split
 test_data = [np.array(x) for x in test_data]
 test_labels = [np.array(x) for x in test_labels]
@@ -27,7 +29,7 @@ print('Training Random Forest Classifier...')
 rfc_classifier = RandomForestClassifier()
 rfc_classifier.fit(train_data, train_labels)
 
-# Evluatiing the Random Forest Classifier on the test data
+# Evaluating the Random Forest Classifier on the test data
 rfc_scores = rfc_classifier.score(test_data, test_labels)
 preds = rfc_classifier.predict(test_data)
 precision, recall, f1, support = precision_recall_fscore_support(test_labels, preds, labels=[0,1,2])
